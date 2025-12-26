791d139f9a89f347ed28a5fb967a49f5a462a1d9
Florian <rogoschflorian@outlook.com>
2025-12-26 20:58:03 +0100

    Initial Commit

---
+++ keep/keepApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepApp.swift
+//  keep
+//
+//  Created by Florian Rogosch on 26.12.25.
+//
+
+import SwiftUI
+
+@main
+struct keepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
